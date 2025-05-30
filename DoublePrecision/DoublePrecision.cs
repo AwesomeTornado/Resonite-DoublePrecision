7d4c00b8e686ad2612a271fcf172eb0bd852f6c4
LazyTeenStudios <harleypeterson@gmail.com>
2025-05-29 21:31:11 -0500

    name changes

---
+++ DoublePrecision/DoublePrecision.cs
@@ -0,0 +1,68 @@
+﻿using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using HarmonyLib;
+using MonkeyLoader.Patching;
+using MonkeyLoader.Resonite;
+using MonkeyLoader.Resonite.Features.FrooxEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityFrooxEngineRunner;
+
+namespace MonkeyLoader.DoublePrecision
+{
+
+    public class AssemblyInfo
+    {
+        //setup instructions (in no particular order):
+        //Replace ExampleURL with your source repo url
+        internal const string VERSION_CONSTANT = "1.0.0"; //Changing the version here updates it in all locations needed
+    }
+    public class DataShare
+    {
+        public static Vector3 CameraPosition = Vector3.zero;
+    };
+
+    [HarmonyPatchCategory(nameof(DoublePrecision))]
+    [HarmonyPatch(typeof(SlotConnector), nameof(SlotConnector.UpdateData))]
+    internal class Slot_Patches : ResoniteMonkey<Slot_Patches>
+    {
+        // The options for these should be provided by your game's game pack.
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches()
+        {
+            yield return new FeaturePatch<ProtofluxTool>(PatchCompatibility.HookOnly);
+        }
+
+        protected DataShare data;
+
+        private static void Postfix(SlotConnector __instance) 
+        {
+            Logger.Info(() => "Slot");
+            //__instance._transform.position += DataShare.CameraPosition;
+            //DataShare.CameraPosition = Vector3.zero;
+        }
+    }
+    [HarmonyPatchCategory(nameof(DoublePrecision))]
+    [HarmonyPatch(typeof(HeadOutput), nameof(HeadOutput.UpdateOverridenView))]
+    internal class Camera_Patches : ResoniteMonkey<Camera_Patches>
+    {
+        // The options for these should be provided by your game's game pack.
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches()
+        {
+            yield return new FeaturePatch<ProtofluxTool>(PatchCompatibility.HookOnly);
+        }
+
+        private static void Postfix(HeadOutput __instance)
+        {
+            DataShare.CameraPosition = __instance.transform.position;
+            __instance.transform.position = Vector3.zero;
+            Logger.Info(() => "test");
+        }
+    }
+
+
+}
---
+++ DoublePrecision/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("DoublePrecision")]
+[assembly: AssemblyProduct("DoublePrecision")]
+[assembly: AssemblyDescription("A mod which fixes floating point errors.")]
+[assembly: AssemblyCompany("__Choco__")]
+[assembly: AssemblyCopyright("Copyright © 2025 __Choco__")]
+[assembly: AssemblyVersion(MonkeyLoader.DoublePrecision.AssemblyInfo.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(MonkeyLoader.DoublePrecision.AssemblyInfo.VERSION_CONSTANT)]

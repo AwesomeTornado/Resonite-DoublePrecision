56e9a52dfedf5ed0149c43d2ff6c7c7598eaeed2
LazyTeenStudios <harleypeterson@gmail.com>
2025-05-31 20:10:29 -0500

    research

--- DoublePrecision/DoublePrecision.cs
+++ DoublePrecision/DoublePrecision.cs
@@ -1,4 +1,5 @@
 ﻿using Elements.Core;
+using FrooxEngine;
 using HarmonyLib;
 using MonkeyLoader.Patching;
 using MonkeyLoader.Resonite;
@@ -30,7 +31,7 @@ namespace MonkeyLoader.DoublePrecision
         private static void Postfix(SlotConnector __instance)
         {
             //Logger.Info(() => "Slot");
-            //__instance._transform.position = DataShare.RootSlotOffset;
+            //__instance._transform.position += DataShare.RootSlotOffset;
             //DataShare.RootSlotOffset = Vector3.zero;
         }
     }
@@ -42,8 +43,13 @@ namespace MonkeyLoader.DoublePrecision
 
         private static void Postfix(HeadOutput __instance)
         {
-            DataShare.RootSlotOffset = new Vector3(0, 00, 10);
-            __instance.transform.position = DataShare.RootSlotOffset;
+            DataShare.GlobalOffset -= new Vector3(0, 1f, 0);
+            DataShare.RootSlotOffset = DataShare.GlobalOffset;
+            //__instance._viewPos += __instance.transform.position.ToEngine();
+            //__instance.transform.position = DataShare.RootSlotOffset;
+            
+
+            //__instance._viewPos = DataShare.RootSlotOffset.ToEngine();
             return;
 
             switch (__instance.Type)
@@ -62,12 +68,29 @@ namespace MonkeyLoader.DoublePrecision
                          __instance.transform.position = Vector3.zero;*/
                         //__instance._viewPos = DataShare.ViewPos;
                         //DataShare.ViewPos = new float3(0, 1f, 0);
-                        DataShare.RootSlotOffset = new Vector3(0, 00010000, 0);
-                        __instance.transform.position = DataShare.RootSlotOffset;
+                        //DataShare.RootSlotOffset = new Vector3(0, 00010000, 0);
+                        //__instance.transform.position = DataShare.RootSlotOffset;
                         //__instance.CameraRoot.position = DataShare.RootSlotOffset;
                         return;
                     }
             }
+        }
+    }
+
+    [HarmonyPatchCategory(nameof(World_Tracking_Offsets))]
+    [HarmonyPatch(typeof(World), nameof(World.UpdateLocalUserOutputPositions))]
+    internal class World_Tracking_Offsets : ResoniteMonkey<World_Tracking_Offsets>
+    {
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
+
+        private static void Postfix(World __instance)
+        {
+            float x = DataShare.RootSlotOffset.x;
+            float y = DataShare.RootSlotOffset.y;
+            float z = DataShare.RootSlotOffset.z;
+            float3 xyz = new float3 (x, y, z);
+            __instance.InputInterface.CustomTrackingOffset += xyz;
+            DataShare.RootSlotOffset = Vector3.zero;
         }
     }
 
@@ -82,6 +105,9 @@ namespace MonkeyLoader.DoublePrecision
  * __Instance.transform.position DOES cause floating point errors! (In the DASH)
  * __Instance.transform.position is VERY IMPORTANT for VR Camera positioning.
  *  It seems to controll the player root position? playspace motion still works, but controller motion does not.
+ *  
+ *  ViewPos -= Transform.position makes screen camera stay still, while avatar moves
+ *  ViewPos += Transform.position makes avatar stay still, while player camera moves
  * 
  * 
  */

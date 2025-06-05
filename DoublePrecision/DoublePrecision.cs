d23ab6241931a45aaca62e12382d5d3f0264c2eb
LazyTeenStudios <harleypeterson@gmail.com>
2025-06-05 15:02:29 -0500

    fix for #3

--- DoublePrecision/DoublePrecision.cs
+++ DoublePrecision/DoublePrecision.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityFrooxEngineRunner;
+using System;
+using UnityEngine.Assertions;
 
 namespace MonkeyLoader.DoublePrecision
 {
@@ -19,34 +21,11 @@ namespace MonkeyLoader.DoublePrecision
     public class DataShare
     {
         public static List<WorldConnector> worldConnectors = new List<WorldConnector>();
+        public static List<World> frooxWorlds = new List<World>();
+        public static List<GameObject> unityWorldRoots = new List<GameObject>();
         public static Vector3 FrooxEngineCameraPosition = Vector3.zero;
         public static Vector3 UnityEngineWorldRootOffset = Vector3.zero;
         public static bool IsUserspaceInitialized = false;
-    }
-
-
-    [HarmonyPatchCategory(nameof(WorldConnectorInit))]
-    [HarmonyPatch(typeof(WorldConnector), nameof(WorldConnector.Initialize))]
-    internal class WorldConnectorInit : ResoniteMonkey<WorldConnectorInit>
-    {
-        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
-
-        private static void Postfix(WorldConnector __instance)
-        {
-            Logger.Info(() => "Called function to init world");
-            if (DataShare.IsUserspaceInitialized)
-            {
-                DataShare.worldConnectors.Add(__instance);
-            }
-            else
-            {
-                Logger.Info(() => "First init, assuming this world is Userspace, and skipping.");
-                DataShare.IsUserspaceInitialized = true;
-            }
-
-            Logger.Info(() => "There are a total of " + DataShare.worldConnectors.Count + " world connectors in the list.");
-
-        }
     }
 
 
@@ -62,6 +41,40 @@ namespace MonkeyLoader.DoublePrecision
                 worldConnector.WorldRoot.transform.position -= DataShare.UnityEngineWorldRootOffset;
             }
             DataShare.UnityEngineWorldRootOffset = Vector3.zero;
+        }
+    }
+
+
+    [HarmonyPatchCategory(nameof(WorldInitIntercept))]
+    [HarmonyPatch(typeof(World), MethodType.Constructor, new Type[] { typeof(WorldManager), typeof(bool), typeof(bool) })]
+    internal class WorldInitIntercept : ResoniteMonkey<WorldInitIntercept>
+    {
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
+        private static void Postfix(World __instance)
+        {
+            Logger.Info(() => "Intercepted World Init, attempting to cache World reference.");
+            if (DataShare.IsUserspaceInitialized)
+            {
+                DataShare.frooxWorlds.Add(__instance);
+                WorldConnector? worldConnector = __instance.Connector as WorldConnector;
+                if (worldConnector is not null)
+                {
+                    DataShare.unityWorldRoots.Add(worldConnector.WorldRoot);
+                }
+                else
+                {
+                    Logger.Error(() => "Unable to cast IWorldConnector to WorldConnector.");
+                }
+
+            }
+            else
+            {
+                Logger.Info(() => "First init, assuming this world is Userspace, and skipping.");
+                DataShare.IsUserspaceInitialized = true;
+            }
+
+            Logger.Info(() => "Done! There are a total of " + DataShare.frooxWorlds.Count + " world connectors in the list.");
+            Debug.Assert(DataShare.frooxWorlds.Count == DataShare.unityWorldRoots.Count); //It's bad if these two get out of sync.
         }
     }
 
@@ -91,6 +104,19 @@ namespace MonkeyLoader.DoublePrecision
                         break;
                     }
             }
+
+            for(int i = 0; i < DataShare.unityWorldRoots.Count; i++)
+            {
+                if (DataShare.frooxWorlds[i] is null || DataShare.frooxWorlds[i].IsDestroyed || DataShare.frooxWorlds[i].IsDisposed)
+                {
+                    DataShare.frooxWorlds.RemoveAt(i);
+                    DataShare.unityWorldRoots.RemoveAt(i);
+                } else if (DataShare.frooxWorlds[i].Focus == World.WorldFocus.Focused)
+                {
+                    DataShare.unityWorldRoots[i].transform.position -= DataShare.UnityEngineWorldRootOffset;
+                }
+            }
+            DataShare.UnityEngineWorldRootOffset = Vector3.zero;
         }
     }
 }

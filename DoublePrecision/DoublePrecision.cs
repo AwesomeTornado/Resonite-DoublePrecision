b2987cdffc95a60a57fe570b6a1a908166c4812a
LazyTeenStudios <harleypeterson@gmail.com>
2025-06-04 20:33:39 -0500

    version number

--- DoublePrecision/DoublePrecision.cs
+++ DoublePrecision/DoublePrecision.cs
@@ -13,7 +13,7 @@ namespace MonkeyLoader.DoublePrecision
 
     public class AssemblyInfo
     {
-        internal const string VERSION_CONSTANT = "1.0.1"; //Changing the version here updates it in all locations needed
+        internal const string VERSION_CONSTANT = "1.1.0"; //Changing the version here updates it in all locations needed
     }
 
     public class DataShare

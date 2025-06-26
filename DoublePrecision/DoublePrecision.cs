034524ddca6f97d9cf80c5abe9bc5a51ebeda717
LazyTeenStudios <harleypeterson@gmail.com>
2025-06-25 20:01:41 -0500

    Finalizations for release.

--- DoublePrecision/DoublePrecision.cs
+++ DoublePrecision/DoublePrecision.cs
@@ -13,7 +13,7 @@ namespace MonkeyLoader.DoublePrecision
 {
     public class AssemblyInfo
     {
-        internal const string VERSION_CONSTANT = "1.5.0"; //Changing the version here updates it in all locations needed
+        internal const string VERSION_CONSTANT = "1.6.0"; //Changing the version here updates it in all locations needed
     }
 
     public class DataShare
@@ -23,58 +23,8 @@ namespace MonkeyLoader.DoublePrecision
         public static List<Vector3> FrooxCameraPosition = new List<Vector3>();
         public static List<PBS_TriplanarMaterial> FrooxMaterials = new List<PBS_TriplanarMaterial>();
         public static List<MaterialProperty?> MaterialIndexes = new List<MaterialProperty?>();
-    }
 
-    [HarmonyPatchCategory(nameof(WorldInitIntercept))]//commment this out, then uncomment after game is loaded
-    [HarmonyPatch(typeof(World), MethodType.Constructor, new Type[] { typeof(WorldManager), typeof(bool), typeof(bool) })]
-    internal class WorldInitIntercept : ResoniteMonkey<WorldInitIntercept>
-    {
-        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
-
-        private static int IsUserspaceInitialized = 1;
-        private static bool ManualMutexErrorCatch = false;
-        public static void Postfix(World __instance)
-        {
-            if (ManualMutexErrorCatch)
-            {
-                Logger.Error(() => "Something went very wrong in the previous world init.");
-            }
-            ManualMutexErrorCatch = true;
-            Logger.Info(() => "Intercepted World Init, attempting to cache World reference.");
-            if (IsUserspaceInitialized >= 1)
-            {
-                WorldConnector? worldConnector = __instance.Connector as WorldConnector;
-                if (worldConnector is not null)
-                {
-                    DataShare.frooxWorlds.Add(__instance);
-                    DataShare.unityWorldRoots.Add(worldConnector.WorldRoot);
-                    DataShare.FrooxCameraPosition.Add(Vector3.zero);
-                }
-                else
-                {
-                    Logger.Error(() => "Unable to cast IWorldConnector to WorldConnector.");
-                }
-
-            }
-            else
-            {
-                Logger.Info(() => "First init, assuming this world is Userspace, and skipping.");
-                IsUserspaceInitialized++;
-            }
-
-            Logger.Info(() => "Done! There are a total of " + DataShare.frooxWorlds.Count + " world connectors in the list.");
-            ManualMutexErrorCatch = false;
-        }
-    }
-
-
-    [HarmonyPatchCategory(nameof(Camera_Patches))]
-    [HarmonyPatch(typeof(HeadOutput), nameof(HeadOutput.UpdatePositioning))]
-    internal class Camera_Patches : ResoniteMonkey<Camera_Patches>
-    {
-        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
-
-        private static void Postfix(HeadOutput __instance)
+        public static int FocusedWorld()
         {
             int index = -1;
             for (int i = 0; i < DataShare.unityWorldRoots.Count; i++)
@@ -92,53 +42,73 @@ namespace MonkeyLoader.DoublePrecision
             }
             if (index == -1)
             {
-                Logger.Error(() => "There are no valid focused worlds! Fatal error, Attepting worlds list regeneration.");
                 World w = Userspace.UserspaceWorld;
                 var worldsList = w.WorldManager.Worlds;
                 foreach (World world in worldsList)
                 {
                     WorldInitIntercept.Postfix(world);
                 }
-                return;
+                return -1;
             }
-            //Vector3 xyz = __instance.transform.localScale;
-            //Vector3 iscl = new Vector3(1 / xyz.x, 1 / xyz.y, 1 / xyz.z);
+            return index;
+        }
+    }
+
+    [HarmonyPatchCategory(nameof(WorldInitIntercept))]
+    [HarmonyPatch(typeof(World), MethodType.Constructor, new Type[] { typeof(WorldManager), typeof(bool), typeof(bool) })]
+    internal class WorldInitIntercept : ResoniteMonkey<WorldInitIntercept>
+    {
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
+
+        public static void Postfix(World __instance)
+        {
+            Logger.Info(() => "Intercepted World Init, attempting to cache World reference.");
+            WorldConnector? worldConnector = __instance.Connector as WorldConnector;
+            if (worldConnector is not null)
+            {
+                DataShare.frooxWorlds.Add(__instance);
+                DataShare.unityWorldRoots.Add(worldConnector.WorldRoot);
+                DataShare.FrooxCameraPosition.Add(Vector3.zero);
+            }
+            else
+            {
+                Logger.Error(() => "Unable to cast IWorldConnector to WorldConnector.");
+            }
+
+            Logger.Info(() => "Done! There are a total of " + DataShare.frooxWorlds.Count + " world connectors in the list.");
+        }
+    }
+
+
+    [HarmonyPatchCategory(nameof(Camera_Patches))]
+    [HarmonyPatch(typeof(HeadOutput), nameof(HeadOutput.UpdatePositioning))]
+    internal class Camera_Patches : ResoniteMonkey<Camera_Patches>
+    {
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
+
+        private static void Postfix(HeadOutput __instance)
+        {
+            int index = DataShare.FocusedWorld();
+            if (index == -1) return;
+
             Vector3 playerMotion = __instance.transform.position - DataShare.FrooxCameraPosition[index];
             DataShare.FrooxCameraPosition[index] = __instance.transform.position;
             Vector3 pos = __instance.transform.position;
             __instance._viewPos -= new float3(pos.x, pos.y, pos.z);
             //Do we really need viewScale?
             DataShare.unityWorldRoots[index].transform.position -= playerMotion;
-
-            //DataShare.unityWorldRoots[index].transform.localScale = iscl;
-            //float3 vpos = __instance._viewPos;
-            //float3 fxyzpos = new float3(vpos.x, iscl.y * vpos.y, vpos.z);
-            //__instance._viewPos = fxyzpos;
-            //__instance._viewScl = new float3(10,10,10);
             __instance.transform.position = Vector3.zero;
             Vector3 rootPos = DataShare.unityWorldRoots[index].transform.position;
             for (int i = 0; i < DataShare.FrooxMaterials.Count; i++)
             {
                 if (DataShare.FrooxMaterials[i] is not null)
                 {
-                    if (DataShare.FrooxMaterials[i].Asset is null)
-
+                    if (DataShare.FrooxMaterials[i].Asset is not null)
                     {
-                        //Logger.Info(() => "FrooxMaterials.Asset is null, index of " + i);
-                        //DataShare.FrooxMaterials.RemoveAt(i);//TODO: Don't remove it, I think it takes a bit to initialize.
-                    }
-                    else
-                    {
-                        //Logger.Info(() => DataShare.MaterialIndexes[i] + i);
-                        int end = DataShare.FrooxMaterials[i]._asset.GetUnity().shader.GetPropertyCount();
                         DataShare.FrooxMaterials[i].Asset.GetUnity().SetVector("_WorldOffset", rootPos);
-                        //EWWWWWW HIGHLY NESTED STATEMENT WARNING, TODO: FIX THIS
                     }
                 }
-                else
-                {
-                    Logger.Info(() => "FrooxMaterials is null, index of " + i);
-                }
+                //TODO: Add cache invalidation for FrooxMaterials.
             }
         }
     }
@@ -146,8 +116,6 @@ namespace MonkeyLoader.DoublePrecision
     [HarmonyPatchCategory(nameof(PBS_Tri_Metal_Overhaul))]
     internal class PBS_Tri_Metal_Overhaul : ResoniteMonkey<PBS_Tri_Metal_Overhaul>
     {
-
-
         protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
 
         [HarmonyPatch(typeof(PBS_TriplanarMetallic), nameof(PBS_TriplanarMetallic.GetShader))]
@@ -176,19 +144,6 @@ namespace MonkeyLoader.DoublePrecision
                 ((StaticShader)__instance._regular.Target).URL.ForceSet(URL);
             }
             return false; //never run original function
-        }
-
-        [HarmonyPatch(typeof(PBS_TriplanarMetallic), nameof(PBS_TriplanarMetallic.InitializeSyncMembers))]//TODO: this entire patch can likely be remvoed.
-        private static void Postfix(PBS_TriplanarMetallic __instance)
-        {
-            var w = Userspace.UserspaceWorld;
-            w.RunInUpdates(5, () =>
-            {
-                __instance._regular.IsDrivable = true;
-                __instance._regular.DriveFrom(__instance._regular, true);
-                __instance._transparent.IsDrivable = true;
-                __instance._transparent.DriveFrom(__instance._transparent, true);
-            });
         }
     }
 
@@ -224,18 +179,19 @@ namespace MonkeyLoader.DoublePrecision
             }
             return false; //never run original function
         }
+    }
 
-        [HarmonyPatch(typeof(PBS_TriplanarSpecular), nameof(PBS_TriplanarSpecular.InitializeSyncMembers))]//TODO: this entire patch can likely be removed.
-        private static void Postfix(PBS_TriplanarSpecular __instance)
+    [HarmonyPatchCategory(nameof(RenderingRepositioning))]
+    [HarmonyPatch(typeof(RenderConnector), nameof(RenderConnector.RenderImmediate))]
+    internal class RenderingRepositioning : ResoniteMonkey<RenderingRepositioning>
+    {
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
+        private static void Prefix(RenderConnector __instance, global::FrooxEngine.RenderSettings renderSettings)
         {
-            var w = Userspace.UserspaceWorld;
-            w.RunInUpdates(5, () =>
-            {
-                __instance._regular.IsDrivable = true;
-                __instance._regular.DriveFrom(__instance._regular, true);
-                __instance._transparent.IsDrivable = true;
-                __instance._transparent.DriveFrom(__instance._transparent, true);
-            });
+            int index = DataShare.FocusedWorld();
+            if (index == -1) return;
+            Vector3 xyz = DataShare.unityWorldRoots[index].transform.position;
+            renderSettings.position += new float3(xyz.x, xyz.y, xyz.z);
         }
     }
 
@@ -246,12 +202,9 @@ namespace MonkeyLoader.DoublePrecision
         protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
         private static void Postfix(PBS_TriplanarMaterial __instance)
         {
-            //Logger.Debug(() => "New Material initialized!");
             DataShare.FrooxMaterials.Add(__instance);
         }
-
     }
-
 }
 
 /*
